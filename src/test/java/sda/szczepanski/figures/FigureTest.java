51b85bcf2bbdd3eaf1372002c91ce1aea564eeb8
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-08-01 15:32:36 +0200

    comment added

--- src/test/java/sda/szczepanski/figures/FigureTest.java
+++ src/test/java/sda/szczepanski/figures/FigureTest.java
@@ -7,13 +7,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class FigureTest {
 
     public static final String EXPECTED_DESCRIPTION = "Figure\npole 12.0\nobwód 16.0";
     @Mock
+    // zastepuje         testFigureMock = Mockito.mock(Figure.class);
     private Figure testFigureMock;
 
     @BeforeEach
