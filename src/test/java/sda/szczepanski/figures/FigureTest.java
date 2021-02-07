07b3f3b33f46a086498ae807f7c099fcd73186e9
Marek Szczepanski <mszczepanski2@atlassian.com>
2021-02-07 16:10:07 +0100

    * FigureTest cleaned

--- src/test/java/sda/szczepanski/figures/FigureTest.java
+++ src/test/java/sda/szczepanski/figures/FigureTest.java
@@ -1,13 +1,11 @@
 package sda.szczepanski.figures;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
