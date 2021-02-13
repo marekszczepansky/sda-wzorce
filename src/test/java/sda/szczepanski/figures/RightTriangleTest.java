806a3e2ed902dc1baeee042fe1c99ecb3e217f61
Marek Szczepanski <mszczepanski2@atlassian.com>
2021-02-13 15:37:53 +0100

    * RightTriangleTest updated * SquareTest updated

--- src/test/java/sda/szczepanski/figures/RightTriangleTest.java
+++ src/test/java/sda/szczepanski/figures/RightTriangleTest.java
@@ -11,6 +11,10 @@ class RightTriangleTest {
     private static final double TEST_HEIGHT = 4;
     private static final int EXPECTED_AREA = 6;
     private static final int EXPECTED_PERIMETER = 12;
+    private static final double TEST_X = 10.0;
+    private static final double TEST_Y = 20.0;
+    private static final Colourable.Colour TEST_COLOUR = Colourable.Colour.RED;
+
     private RightTriangle testTriangle;
 
     @BeforeEach
@@ -30,5 +34,20 @@ class RightTriangleTest {
         final double actualArea = testTriangle.calculateArea();
 
         assertEquals(EXPECTED_AREA, actualArea);
+    }
+
+    @Test
+    void shouldSetColour() {
+        testTriangle.setColour(TEST_COLOUR);
+
+        assertEquals(TEST_COLOUR, testTriangle.getColour());
+    }
+
+    @Test
+    void shouldSetCoordinates() {
+        testTriangle.setCoordinates(TEST_X, TEST_Y);
+
+        assertEquals(TEST_X, testTriangle.getX());
+        assertEquals(TEST_Y, testTriangle.getY());
     }
 }
--- src/test/java/sda/szczepanski/figures/SquareTest.java
+++ src/test/java/sda/szczepanski/figures/SquareTest.java
@@ -9,6 +9,10 @@ class SquareTest {
     private static final double TEST_SIDE = 3;
     private static final double EXPECTED_AREA = 9;
     private static final double EXPECTED_PERIMETER = 12;
+    private static final double TEST_X = 10.0;
+    private static final double TEST_Y = 20.0;
+    private static final Colourable.Colour TEST_COLOUR = Colourable.Colour.RED;
+
     private Square testSquare;
 
     @BeforeEach
@@ -23,5 +27,20 @@ class SquareTest {
     @Test
     void shouldCalculateArea() {
         assertEquals(EXPECTED_AREA, testSquare.calculateArea());
+    }
+
+    @Test
+    void shouldSetColour() {
+        testSquare.setColour(TEST_COLOUR);
+
+        assertEquals(TEST_COLOUR, testSquare.getColour());
+    }
+
+    @Test
+    void shouldSetCoordinates() {
+        testSquare.setCoordinates(TEST_X, TEST_Y);
+
+        assertEquals(TEST_X, testSquare.getX());
+        assertEquals(TEST_Y, testSquare.getY());
     }
 }
