2da30c2ae588bbcf9c0357f5ce072e8de9313645
Marek Szczepanski <mszczepanski2@atlassian.com>
2021-03-07 10:26:32 +0100

    RightTriangle builder added

--- src/main/java/sda/szczepanski/figures/RightTriangle.java
+++ src/main/java/sda/szczepanski/figures/RightTriangle.java
@@ -15,6 +15,14 @@ public class RightTriangle implements SuperFigure{
         this.height = height;
     }
 
+    private RightTriangle(Builder builder) {
+        this.base = builder.base;
+        this.colour = builder.colour;
+        this.height = builder.height;
+        this.x = builder.x;
+        this.y = builder.y;
+    }
+
     @Override
     public double calculatePerimeter() {
         return base + height + sqrt(base * base + height * height);
@@ -54,5 +62,41 @@ public class RightTriangle implements SuperFigure{
     @Override
     public double getY() {
         return y;
+    }
+
+    public static class Builder {
+
+        private double height;
+        private double base;
+        private Colour colour;
+        private double x;
+        private double y;
+
+        public RightTriangle build() {
+            if (height <= 0 || base <= 0) {
+                throw new IllegalStateException("Base and height must be greater than 0");
+            }
+            return new RightTriangle(this);
+        }
+
+        public Builder withBase(double base) {
+            this.base = base;
+            return this;
+        }
+        public Builder withHeight(double height) {
+            this.height = height;
+            return this;
+        }
+
+        protected Builder withColour(Colour colour) {
+            this.colour = colour;
+            return this;
+        }
+
+        protected Builder withCoordinates(double x, double y) {
+            this.x = x;
+            this.y = y;
+            return this;
+        }
     }
 }
